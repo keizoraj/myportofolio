a3c36a1d128c48bd1877a9a6776781cef67ba6a3
keizoraj <keizora.jelita@ui.ac.id>
2026-09-14 20:17:16 +0700

    feat: Tugas 2 Add Project model

--- main/models.py
+++ main/models.py
@@ -19,9 +19,20 @@ class Experience(models.Model):
     thumbnail = models.URLField(blank=True, null=True)
     started_at = models.DateTimeField(auto_now_add=True)
     ended_at = models.DateTimeField(blank=True, null=True)
+
     def __str__(self):
         return self.title
     
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
