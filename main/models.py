2633d7ce5f8125023c1a18a176432af8fad00950
keizoraj <keizora.jelita@ui.ac.id>
2026-09-16 22:49:14 +0700

    feat: Tutorial 3

--- main/models.py
+++ main/models.py
@@ -31,8 +31,9 @@ class Project(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    category = models.CharField(max_length=100)
-    year = models.IntegerField()
-
+    tech_stack = models.CharField(max_length=255, null=True, blank=True)
+    project_url = models.URLField(blank=True)
+    project_image_url = models.URLField(blank=True, max_length=500)
+    
     def __str__(self):
         return self.title
