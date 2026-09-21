f6fa072a60af2fb37d2e59a666a93af6d921e21d
keizoraj <keizora.jelita@ui.ac.id>
2026-09-21 21:09:36 +0700

    Add Education CRUD and JSON views

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,78 @@
 from django.urls import path
-from main.views import show_main, show_experience, show_projects, create_project, get_projects_json, delete_project
+from main.views import (
+    show_main, 
+    show_experience, 
+    show_projects, 
+    create_project, 
+    get_projects_json, 
+    delete_project,
+    create_experience,
+    update_experience,
+    delete_experience,
+    get_experience_json,
+    show_education,
+    create_education,
+    update_education,
+    delete_education,
+    get_education_json,
+)
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
+    path("experience/add/", create_experience, name="create_experience"),
+    path(
+        "experience/<uuid:experience_id>/edit/",
+        update_experience,
+        name="update_experience",
+    ),
+    path(
+        "experience/<uuid:experience_id>/delete/",
+        delete_experience,
+        name="delete_experience",
+    ),
+    path(
+        "api/experience/",
+        get_experience_json,
+        name="get_experience_json",
+    ),
     path("projects/", show_projects, name="show_projects"),
     path("projects/add/", create_project, name="create_project"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
-    path("projects/<uuid:project_id>/delete/", delete_project,name="delete_project"),
+    path(
+        "projects/<uuid:project_id>/delete/", 
+        delete_project, 
+        name="delete_project"
+    ),
+    path(
+    "education/",
+    show_education,
+    name="show_education",
+    ),
+
+    path(
+        "education/add/",
+        create_education,
+        name="create_education",
+    ),
+
+    path(
+        "education/<uuid:education_id>/edit/",
+        update_education,
+        name="update_education",
+    ),
+
+    path(
+        "education/<uuid:education_id>/delete/",
+        delete_education,
+        name="delete_education",
+    ),
+
+    path(
+        "api/education/",
+        get_education_json,
+        name="get_education_json",
+    ),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from main.models import Experience, Project
-from main.forms import ProjectForm
+from django.shortcuts import get_object_or_404, redirect,render
+from main.models import Experience, Project, Education
+from main.forms import ProjectForm, ExperienceForm, EducationForm
 from django.contrib import messages
 from django.core import serializers
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+
 
 
 def show_main(request):
@@ -21,11 +21,194 @@ def show_main(request):
 
 
 def show_experience(request):
+    json_response = get_experience_json(request)
+
+    experiences = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+
+    experiences = [experience.object for experience in experiences]
+
     context = {
         "name": "Keizora",
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def create_experience(request):
+    form = ExperienceForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience baru berhasil ditambahkan!")
+        return redirect("main:show_experience")
+
+    context = {
+        "name": "Keizora",
+        "form": form,
+        "form_title": "Tambah Experience",
+        "submit_text": "Tambah Experience",
+    }
+
+    return render(request, "experience_form.html", context)
+
+def update_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+
+    form = ExperienceForm(
+        request.POST or None,
+        instance=experience
+    )
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Experience berhasil diperbarui!")
+        return redirect("main:show_experience")
+
+    context = {
+        "name": "Keizora",
+        "form": form,
+        "form_title": "Edit Experience",
+        "submit_text": "Simpan Perubahan",
+    }
+
+    return render(request, "experience_form.html", context)
+
+def delete_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+
+    if request.method == "POST":
+        experience.delete()
+        messages.success(request, "Experience berhasil dihapus!")
+
+    return redirect("main:show_experience")
+
+
+def get_experience_json(request):
+    experiences = Experience.objects.all()
+
+    experiences_json = serializers.serialize(
+        "json",
+        experiences
+    )
+
+    return HttpResponse(
+        experiences_json,
+        content_type="application/json"
+    )
+
+def show_education(request):
+    json_response = get_education_json(request)
+
+    education_list = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+
+    education_list = [
+        education.object
+        for education in education_list
+    ]
+
+    context = {
+        "name": "Keizora",
+        "education_list": education_list,
+    }
+
+    return render(
+        request,
+        "education.html",
+        context
+    )
+
+
+def create_education(request):
+    form = EducationForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(
+            request,
+            "Pendidikan berhasil ditambahkan!"
+        )
+        return redirect("main:show_education")
+
+    context = {
+        "name": "Keizora",
+        "form": form,
+        "form_title": "Tambah Pendidikan",
+        "submit_text": "Tambah Pendidikan",
+    }
+
+    return render(
+        request,
+        "education_form.html",
+        context
+    )
+
+
+def update_education(request, education_id):
+    education = get_object_or_404(
+        Education,
+        pk=education_id
+    )
+
+    form = EducationForm(
+        request.POST or None,
+        instance=education
+    )
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(
+            request,
+            "Pendidikan berhasil diperbarui!"
+        )
+        return redirect("main:show_education")
+
+    context = {
+        "name": "Keizora",
+        "form": form,
+        "form_title": "Edit Pendidikan",
+        "submit_text": "Simpan Perubahan",
+    }
+
+    return render(
+        request,
+        "education_form.html",
+        context
+    )
+
+
+def delete_education(request, education_id):
+    education = get_object_or_404(
+        Education,
+        pk=education_id
+    )
+
+    if request.method == "POST":
+        education.delete()
+        messages.success(
+            request,
+            "Pendidikan berhasil dihapus!"
+        )
+
+    return redirect("main:show_education")
+
+
+def get_education_json(request):
+    education_list = Education.objects.all()
+
+    education_json = serializers.serialize(
+        "json",
+        education_list
+    )
+
+    return HttpResponse(
+        education_json,
+        content_type="application/json"
+    )
 
 def show_projects(request):
     json_response = get_projects_json(request)
